1f9532ef3853eab297919ddb92b77caefce0d661
Michael <Mike.github@gmx.de>
2025-01-15 19:38:44 +0100

    commit changes
    Made major changes on the website

--- script.js
+++
@@ -1,5 +0,0 @@
-/*document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission
-    alert("Thank you for reaching out! We'll get back to you soon.");
-});
-*/
